b855ed2b8d5fd181008cab273f1e09ed546d08d1
Vinzenz Brodschneider <brodschneider.vinzenz@gmail.com>
2017-04-12 15:28:50 +0200

    added archimedean spiral

--- ulamSpiral/js/main.js
+++ ulamSpiral/js/main.js
@@ -2,13 +2,61 @@ var imageData = {};
 var ctx = {};
 var canvas = {};
 
+var $numberOfCircles = $("#numberOfCircles");
+var $stretchiness = $("#stretchiness");
+var $size = $('#size');
+var $archimedeanWrapper = $("#archimedean_wrapper");
 
 var config = {
     size: {
-        size: 250
-    }
+        size: 1000
+    },
+    stretchiness: 0.1,
+    circleAmount: 1500
 };
 
+
+function paintArchimedianSpiral() {
+    ctx.beginPath();
+    ctx.clearRect(0, 0, config.size.size, config.size.size);
+    var centerX = config.size.size / 2;
+
+    var centerY = config.size.size / 2;
+    ctx.moveTo(centerX, centerY);
+    //var amount = 60;
+    //var increment = 2 * Math.PI / amount;
+    //var theta = increment;
+    //while (theta < 100 * Math.PI) {
+    //    var newX = centerX + theta * config.stretchiness * Math.cos(theta);
+    //    var newY = centerY + theta * config.stretchiness * Math.sin(theta);
+    //    ctx.lineTo(newX, newY);
+    //    theta = theta + increment;
+    //}
+    //ctx.stroke();
+
+    ctx.font = "8px Georgia";
+    var cnt = 1;
+    var theta2 = 0;
+    for (var index = 1; index < config.circleAmount; index++) {
+        var amountOfNumbers = Math.pow(index + 1, 2) - Math.pow(index, 2);
+        var increment2 = 2 * Math.PI / amountOfNumbers;
+        var amountOfAngle = (index) * 2 * Math.PI;
+        var numbersDrawn = 0;
+        // there are some rounding errors....
+        while (theta2 < amountOfAngle && !(Math.abs(theta2 - amountOfAngle) < 0.00001)) {
+            theta2 = theta2 + increment2;
+            var newX2 = centerX + theta2 * config.stretchiness * Math.cos(theta2);
+            var newY2 = centerY + theta2 * config.stretchiness * Math.sin(theta2);
+            cnt++;
+            if (isPrime(cnt)) {
+                ctx.rect(newX2, newY2, 1, 1);
+            }
+            numbersDrawn++;
+        }
+
+    }
+    ctx.stroke();
+}
 
 function paintUlamSpiral(data) {
     var coordinate_system = [];
@@ -63,14 +111,41 @@ function paintUlamSpiral(data) {
     });
 }
 
+function updateArchimedeanConfig() {
+    if (config.circleAmount == $numberOfCircles.val() && config.stretchiness == $stretchiness.val()) {
+        return;
+    }
+    config.circleAmount = $numberOfCircles.val();
+    config.stretchiness = $stretchiness.val();
+    updateCanvas();
+}
+
+function initArchimedeanConfig() {
+    $archimedeanWrapper.show();
+    $numberOfCircles.val(config.circleAmount);
+    $stretchiness.val(config.stretchiness);
+}
+function selectSpiral() {
+    var selectedSpiral = $("#spiral_select").val();
+    if (selectedSpiral == 1) {
+        $archimedeanWrapper.hide();
+        paintUlamSpiral(imageData.data);
+        ctx.putImageData(imageData, 0, 0)
+    } else {
+        initArchimedeanConfig();
+        paintArchimedianSpiral();
+    }
+}
 
 function updateCanvas() {
-    paintUlamSpiral(imageData.data);
-    ctx.putImageData(imageData, 0, 0)
+    selectSpiral();
 }
 
 function configChanged() {
-    config.size.size = $('#size').val();
+    if (config.size.size == $size.val()) {
+        return;
+    }
+    config.size.size = $size.val();
     canvas.height = config.size.size;
     canvas.width = config.size.size;
     imageData = ctx.createImageData(config.size.size, config.size.size);
@@ -83,7 +158,9 @@ $(document).ready(function () {
     ctx = canvas.getContext("2d");
     canvas.height = config.size.size;
     canvas.width = config.size.size;
-    $('#size').val(config.size.size);
+    $size.val(config.size.size);
+    initArchimedeanConfig();
+    $archimedeanWrapper.hide();
     imageData = ctx.createImageData(config.size.size, config.size.size);
     updateCanvas();
 });
